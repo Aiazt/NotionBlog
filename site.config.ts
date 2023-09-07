45f4654cc80b8e5fd961cf5759c1f0bd137d787d
偏爱WRC <39250628+Aiazt@users.noreply.github.com>
2023-09-07 14:33:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Azt's Blog',
+  name: 'Azts Blog',
   domain: 'notion.aiazt.net',
   author: 'Aiazt',
 

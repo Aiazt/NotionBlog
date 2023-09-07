716ea57eec896da4cbfd01c6358f77ceeb4a8101
偏爱WRC <39250628+Aiazt@users.noreply.github.com>
2023-09-07 14:41:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,14 +19,9 @@ export default siteConfig({
   socialImageSubtitle: 'Hello World!',
 
   // social usernames (optional)
-  twitter: '', 
+  twitter: '',
   github: 'aiazt',
   linkedin: '',
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`

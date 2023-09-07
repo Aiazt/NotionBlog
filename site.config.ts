02bce499452f288bcec61b32cfca968375be525c
偏爱WRC <39250628+Aiazt@users.noreply.github.com>
2023-09-07 13:50:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,19 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Azt's Blog',
+  domain: 'notion.aiazt.net',
+  author: 'Aiazt',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '相逢的人会再相逢',
+  socialImageTitle: 'Transitive Bullshit',
+  socialImageSubtitle: 'Hello World!',
+
+  // social usernames (optional)
+  twitter: '', 
+  github: 'aiazt',
+  linkedin: '',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
